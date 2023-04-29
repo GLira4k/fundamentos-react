329f6a78b0fa4d8ddffebbc74294d33cf4e774fe
Gabriel Lira <103190663+GLira4k@users.noreply.github.com>
2023-04-28 22:38:18 -0300

    Iniciando Componentização
    Iniciamos a componentização do projeto e utilizamos props dinâmicas.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,19 @@
 import "./styles.css";
+import { Card } from '../../components/Card';
 
 export function Home() {
   return (
-    <div className="container">
-      <h1>Lista de Presença</h1>
-      <input required type="text" className="input" />
-      <button type="button" className="btn">
-        Adicionar
-      </button>
-    </div>
+    <main>
+      <section className="container">
+        <h1>Lista de Presença</h1>
+        <input required type="text" className="input" placeholder="Digite um nome"/>
+        <button type="button" className="btn"> 
+          Adicionar
+        </button>
+        <Card name="Germano" time="09:02:14"/>
+        <Card name="Gabriel" time="10:25:51"/>
+        <Card name="Lucas" time="11:37:44"/> 
+      </section>
+    </main>
   );
 }
