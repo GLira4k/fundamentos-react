3b4ee0a81d99bbe6feb84c343983affc97095a0f
Gabriel Lira <103190663+GLira4k@users.noreply.github.com>
2023-04-28 22:58:37 -0300

    4

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,22 @@ import "./styles.css";
 import { Card } from '../../components/Card';
 
 export function Home() {
+  const studentName= "";
+
+  function handleNameChange(name){
+    studentName = name;
+  }
+
   return (
     <main>
       <section className="container">
         <h1>Lista de Presença</h1>
-        <input required type="text" className="input" placeholder="Digite um nome"/>
+        <input required 
+        type="text" 
+        className="input" 
+        placeholder="Digite um nome"
+        onChange={e => handleNameChange(e.target.value)}
+        />
         <button type="button" className="btn"> 
           Adicionar
         </button>
@@ -15,5 +26,7 @@ export function Home() {
         <Card name="Lucas" time="11:37:44"/> 
       </section>
     </main>
-  );
+ );
 }
+
+//Ao gerar uma alteração de estado ele provoca uma nova renderização.
